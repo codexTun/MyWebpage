c21cf64f2b069a2a76eb598e18a498f08e7b8349
IFEANYI MUNONYE <charlesmuno@yahoo.com>
2017-03-08 10:08:48 -0600

    Home controller set

---
+++ MyWebapp/MyWebapp/Controllers/HomeController.cs
@@ -0,0 +1,5 @@
+﻿using System; using System.Collections.Generic; using System.Linq; using System.Threading.Tasks; using Microsoft.AspNetCore.Mvc;  namespace MyWebapp.Controllers
+{  	public class HomeController : Controller 	{
+		//GET : Home
+		public ActionResult Index()
+		{ 			return View(); 		} 	}  } 
